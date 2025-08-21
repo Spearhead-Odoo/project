13353d7364fadd30bc4a17a51e93e856e5c2830f
ricardoMC <rmaldonado@spearhead.global>
2025-08-21 18:41:13 -0400

    [MIG] project_status: Migration to version 18.0

--- project_status/models/project.py
+++ project_status/models/project.py
@@ -1,13 +1,18 @@
-from odoo import SUPERUSER_ID, api, fields, models
+from odoo import SUPERUSER_ID, fields, models
 
 
 class Project(models.Model):
     _inherit = "project.project"
 
-    @api.model
-    def _read_group_status_ids(self, statuses, domain, order):
-        statuse_ids = statuses._search([], order=order, access_rights_uid=SUPERUSER_ID)
-        return statuses.browse(statuse_ids)
+    @staticmethod
+    def _read_group_status_ids(values, domain, order, *args, **kwargs):
+        env = values.env if hasattr(values, "env") else values._env
+        if not isinstance(domain, list):
+            domain = []
+        status_ids = (
+            env["project.status"].with_user(SUPERUSER_ID)._search(domain, order=order)
+        )
+        return env["project.status"].browse(status_ids)
 
     project_status = fields.Many2one(
         comodel_name="project.status",
